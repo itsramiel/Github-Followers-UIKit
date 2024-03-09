b2acc6040cc0c5d6da57bb33f8836fe8518e275d
Rami Elwan <ramielwan48@gmail.com>
2024-03-09 12:37:37 +0100

    feat: setup basic app navigation

--- GhFollowers/FavoritesListVC.swift
+++ GhFollowers/FavoritesListVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  FavoritesListVC.swift
 //  GhFollowers
 //
 //  Created by Rami Elwan on 08.03.24.
@@ -7,13 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FavoritesListVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        view.backgroundColor = .systemBlue
     }
-
-
+    
 }
-
---
+++ GhFollowers/SearchVC.swift
@@ -0,0 +1,17 @@
+//
+//  SearchVC.swift
+//  GhFollowers
+//
+//  Created by Rami Elwan on 08.03.24.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemPink
+    }
+}
